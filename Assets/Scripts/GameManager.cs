cf4a12ceb9c5bd40099d0920948a1edb6311e240
Nickita45 <supernikita45@gmail.com>
2023-11-13 00:12:34 +0100

    added main menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,19 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     private int currentScenarios = 0;
-    //private string[] nameScenarios = {"Scene1","Scene2","Scene3Borik","Scene3BorikAfter","Scene4Dmitry","Scene4DmitryAfter","Scene5MaksymIlya","Scene5MaksymIlyaAfter","Scene6"};
-    private string[] nameScenarios = {"Scene3BorikAfter","Scene4DmitryAfter","Scene5MaksymIlyaAfter","Scene6"};
+    private string[] nameScenarios = {"Scene1","Scene2","Scene3Borik","Scene3BorikAfter","Scene4Dmitry","Scene4DmitryAfter","Scene5MaksymIlya","Scene5MaksymIlyaAfter","Scene6","Credits"};
+    //private string[] nameScenarios = {"Scene4DmitryAfter","Credits"};
     
     private static GameManager _instance;
     public static GameManager Instance => _instance;
-    public GameObject credits, maingame;
+    public GameObject credits, maingame, menu;
 
     public TextPlayers textPlayers;
+    public TMP_InputField inputField;
     // Start is called before the first frame update
     // Update is called once per frame
     void Awake()
@@ -25,12 +28,12 @@ public class GameManager : MonoBehaviour
             return;
         }
         _instance = this;
-
-        UpdateScenarios();
+        inputField.onEndEdit.AddListener(new UnityAction<string>(StartGame));
+        // UpdateScenarios();
     }
     public void UpdateScenarios()
     {
-        if(currentScenarios == nameScenarios.Length)
+        if(nameScenarios[currentScenarios] == "Credits")
         {
             //final 
             credits.SetActive(true);
@@ -41,5 +44,26 @@ public class GameManager : MonoBehaviour
             textPlayers.startReadingText(nameScenarios[currentScenarios]);
             currentScenarios++;
         }
+    }
+    public void StartGame(string scene)
+    {
+        Debug.Log(scene);
+        menu.SetActive(false);
+
+        maingame.SetActive(true);
+
+        currentScenarios = FindStringIndex(nameScenarios,scene);
+        UpdateScenarios();
+    }
+    static int FindStringIndex(string[] array, string target)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] == target)
+            {
+                return i; // Если нашли, возвращаем индекс
+            }
+        }
+        return 0; // Если не нашли, возвращаем -1
     }
 }
